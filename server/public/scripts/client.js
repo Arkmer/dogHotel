c4cee245a849b8a32de1aabae043a53fda2f5926
Sam Standing <samzstanding@gmail.com>
2018-02-20 11:59:44 -0600

    added get/post for owners

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,42 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jQ');
+
+}
+
+function postOwner () {
+    let owner = {
+        firstName: $('#firstNameIn'),
+        lastName: $('#lastNameIn')
+    };
+    $.ajax({
+        type: 'POST',
+        url:'/pets',
+        data: owner
+    }).done(function (response) {
+        console.log('sending owner info');
+        getPets();
+        clearInputs();
+    })
+}
+
+function getOwners () {
+    $.ajax({
+        type: 'GET',
+        url:'/pets'
+    }).done(function (response) {
+        console.log('got owner info');
+        displayOwners(reponse);
+        clearInputs();
+    }).fail(function (response) {
+        console.log('error:', response);
+    })
+}
+
+displayOwners(array) {
+    let output = //wait for ryan;
+    output.empty();
+    for (owner of array) {
+        
+    }
 }
